b4a5d97ae476c1b548a7f5c40c465a2a2750126c
RANIE SOARES DA COSTA <54813595+ranie2019@users.noreply.github.com>
2020-06-02 15:57:54 -0300

    exercicios

---
+++ 41.py
@@ -0,0 +1,13 @@
+from datetime import date
+atual = date.today().year
+nasc = int(input('Ano de Nascimento: '))
+idade = atual - nasc
+print(f'Quem Nasceu Em {nasc} tem anos em {idade} Em {atual}')
+if idade == 18:
+    print('voce Tem Que Se Alistar Imediatamente')
+elif idade < 18:
+    saldo = 18 - idade
+    print(f'Ainda Falta {saldo}')
+elif idade > 18:
+    saldo = idade - 18
+    print(f'Voce Ja Deveria Ter Se Alistado Ha {saldo} Anos')
